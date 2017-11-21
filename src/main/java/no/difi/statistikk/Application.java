684695227644b92bd2b819654d8f3edfc50b3d42
Thomas Johansen <Thomas.Johansen@difi.no>
2017-11-21 11:59:00 +0100

    2017-11-21-1158|PBLEID-15261: Spring Boot-skjelett

---
+++ src/main/java/no/difi/statistikk/Application.java
@@ -0,0 +1,21 @@
+package no.difi.statistikk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping
+    String home() {
+        return "Hello World!";
+    }
+
+}
